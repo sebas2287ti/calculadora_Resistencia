d473ddf0ed298a0fc0f41fb89e0e1e6282237651
Sebastian <Sebas2287ti@gmail.com>
2026-04-24 20:47:09 -0500

    Background UI

---
+++ src/com/restcal/infrastructure/ui/windows/Background.java
@@ -0,0 +1,22 @@
+package com.restcal.infrastructure.ui.windows;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class Background extends JPanel {
+    private Image img;
+
+    public Background(String s) {
+        this.img = new ImageIcon(Objects.requireNonNull(getClass().getResource("/assets/images/Resistencia.png"))).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (img != null) {
+            g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+}
--- src/com/restcal/infrastructure/ui/windows/Resistor.java
+++ src/com/restcal/infrastructure/ui/windows/Resistor.java
@@ -5,17 +5,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Resistor {
-
+public class Resistor extends JFrame {
     private JPanel mainPanel;
 
-
     public Resistor() {
-
     }
 
-    public JPanel getMainPanel()
-    {
-        return mainPanel;
+    public JPanel getMainPanel() {
+        Background background = new Background("/assets/images/Resistencia.png");
+        background.setLayout(new BorderLayout());
+
+        if (mainPanel != null) {
+        mainPanel.setOpaque(false);
+        background.add(mainPanel, BorderLayout.CENTER);
+        };
+        return background;
     }
 }
