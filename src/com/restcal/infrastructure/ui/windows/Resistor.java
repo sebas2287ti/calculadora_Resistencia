bbd6c76b5a88e9cbcc2824197eabc5748d9b320d
Sebastian <Sebas2287ti@gmail.com>
2026-04-25 20:54:30 -0500

    Corrections in code Resistor.java

--- src/com/restcal/infrastructure/ui/windows/Resistor.java
+++ src/com/restcal/infrastructure/ui/windows/Resistor.java
@@ -13,19 +13,23 @@ public class Resistor extends JFrame {
     private JComboBox<DataBaseColors> color2;
     private JComboBox<DataBaseColors> multiple1;
     private JComboBox<DataBaseColors> tolerance1;
+    private JButton BotonCalcular;
+    private JLabel OhmiosMax;
+    private JLabel OhmiosMin;
 
     public Resistor() {
-
-        //245, 142 -- 277, 267 color1
-        //298. 142 -- 330, 267 color2
-        //351, 142 -- 384, 267 mult1
-        //515, 142 -- 538. 267 tolerance
-        /*
-        X: 246 , Y: 286
-        X: 554 , Y: 324
-         */
-
+        inicializador();
     }
+
+        private void inicializador () {
+            color1 = UiConstrutor.CrearComboBox(245,277,142,267, DataBaseColors.getSoloDigitos());
+            color2 = UiConstrutor.CrearComboBox(298,330,142,267, DataBaseColors.getSoloDigitos());
+            multiple1 = UiConstrutor.CrearComboBox(351,384,142,267, DataBaseColors.getSoloMultiplicadores());
+            tolerance1 = UiConstrutor.CrearComboBox(502,538,142,267, DataBaseColors.getSoloTolerancias());
+            BotonCalcular = UiConstrutor.CrearBoton(246,554,286,324, "Calcular");
+            OhmiosMax = UiConstrutor.CrearLabel(653,750,153,188, "0Ω");
+            OhmiosMin = UiConstrutor.CrearLabel(653,750,219,254, "0Ω");
+        }
 
     public JPanel getMainPanel() {
         Background background = new Background("/assets/images/Resistencia.png");
@@ -39,22 +43,13 @@ public class Resistor extends JFrame {
             }
         });
 
-
-        JComboBox<DataBaseColors> ComboBoxDigit1 = UiConstrutor.CrearComboBox(245,277,142,267, DataBaseColors.getSoloDigitos());
-        JComboBox<DataBaseColors> ComboBoxDigit2 = UiConstrutor.CrearComboBox(298,330,142,267, DataBaseColors.getSoloDigitos());
-        JComboBox<DataBaseColors> ComboBoxMultiple1 = UiConstrutor.CrearComboBox(351,384,142,267, DataBaseColors.getSoloMultiplicadores());
-        JComboBox<DataBaseColors> ComboBoxTolerance1 = UiConstrutor.CrearComboBox(502,538,142,267, DataBaseColors.getSoloTolerancias());
-        JButton BotonCalcular = UiConstrutor.CrearBoton(246,554,286,324, "Calcular");
-        JLabel OhmiosMax = UiConstrutor.CrearLabel(653,750,153,188, "0Ω");
-        JLabel Ohmiosmin = UiConstrutor.CrearLabel(653,750,219,254, "0Ω");
-
-        background.add(ComboBoxDigit1);
-        background.add(ComboBoxDigit2);
-        background.add(ComboBoxMultiple1);
-        background.add(ComboBoxTolerance1);
+        background.add(color1);
+        background.add(color2);
+        background.add(multiple1);
+        background.add(tolerance1);
         background.add(BotonCalcular);
         background.add(OhmiosMax);
-        background.add(Ohmiosmin);
+        background.add(OhmiosMin);
 
         if (mainPanel != null) {
         mainPanel.setOpaque(false);
