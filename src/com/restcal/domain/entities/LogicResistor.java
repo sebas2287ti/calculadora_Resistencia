3fb9667d18b9c7aec0fde8d0ab6d073da7676dae
Sebastian <Sebas2287ti@gmail.com>
2026-04-25 23:54:25 -0500

    Corretion in LogitResistor

--- src/com/restcal/domain/entities/LogicResistor.java
+++ src/com/restcal/domain/entities/LogicResistor.java
@@ -2,11 +2,18 @@ package com.restcal.domain.entities;
 
 public class LogicResistor {
 
+    private  float ValorNominal;
+
+    public float ValorNominal (float digit1, float digit2, float multiple1) {
+        return ((digit1 * 10 + digit2) * multiple1);
+    }
     public float ResistorOhmiosMax (float digit1, float digit2, float multiple1 ,float tolerance1){
-        return (((digit1 * 10 + digit2) * multiple1) + ((digit1 * 10 + digit2) * multiple1) * tolerance1);
+        float valorNominal = ValorNominal(digit1,digit2,multiple1);
+        return valorNominal + valorNominal * tolerance1;
     }
 
     public float ResistorOhmiosmin (float digit1, float digit2, float multiple1 ,float tolerance1){
-        return (((digit1 * 10 + digit2) * multiple1) - ((digit1 * 10 + digit2) * multiple1) * tolerance1);
+        float valorNominal = ValorNominal(digit1,digit2,multiple1);
+        return valorNominal - valorNominal * tolerance1;
     }
 }
