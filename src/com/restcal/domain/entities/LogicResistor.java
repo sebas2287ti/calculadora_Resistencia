7e62911b83bb8fdb5cdcdafdaa68fe561b22f080
Sebastian <Sebas2287ti@gmail.com>
2026-04-25 20:11:29 -0500

    LogicResistor

--- src/com/restcal/domain/entities/LogicResistor.java
+++ src/com/restcal/domain/entities/LogicResistor.java
@@ -1,4 +1,12 @@
 package com.restcal.domain.entities;
 
 public class LogicResistor {
+
+    public float ResistorOhmiosMax (float digit1, float digit2, float multiple1 ,float tolerance1){
+        return (((digit1 * 10 + digit2) * multiple1) + ((digit1 * 10 + digit2) * multiple1) * tolerance1);
+    }
+
+    public float ResistorOhmiosmin (float digit1, float digit2, float multiple1 ,float tolerance1){
+        return (((digit1 * 10 + digit2) * multiple1) - ((digit1 * 10 + digit2) * multiple1) * tolerance1);
+    }
 }
